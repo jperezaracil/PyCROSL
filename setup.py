be3df9db182984abf32e3df08bab59c217208db3
eugenioLR <eugeniolorente0@gmail.com>
2023-10-10 12:31:16 +0200

    changed version since there are new changes

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='PyCROSL',
-      version='1.0',
+      version='1.1',
       description='Pyhton implementation of the \"Coral Reef Optimization with Substrate Layers\" algorithm',
       long_description=open('README.md', 'r').read(),
       long_description_content_type='text/markdown',
